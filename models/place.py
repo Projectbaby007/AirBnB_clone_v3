a4432b5003d2684d133eb8874d95bf2102744099
pizzosta <pizzostor@gmail.com>
2023-05-31 13:36:31 +0000

    fix PEP8

--- models/place.py
+++ models/place.py
@@ -12,7 +12,8 @@ from models.base_model import BaseModel, Base
 
 if models.storage_t == 'db':
     place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id', String(60), ForeignKey("places.id"),
+                          Column('place_id', String(60),
+                                 ForeignKey("places.id"),
                                  primary_key=True, nullable=False),
                           Column('amenity_id', String(60),
                                  ForeignKey("amenities.id"),
